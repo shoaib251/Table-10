0e5b47580cb5ece7d8a38de5bb618ffcbf43aeb0
shoaib251 <108321150+shoaib251@users.noreply.github.com>
2022-12-07 12:15:02 +0500

    Add files via upload
    10 table

---
+++ java.js
@@ -0,0 +1,13 @@
+function table(){
+
+    let table= document.getElementById('table').value; 
+     
+let showResult;
+    for (  let x=1; x<=10; x++)
+     {
+        showResult =table + " x "+x+" = "+table*x;
+       let result=<h3>showResult</h3> ;
+     
+      
+    }  document.getElementById('h1').innerHTML=result;  
+}
